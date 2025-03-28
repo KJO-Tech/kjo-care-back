252b99a1cfe2dffec9c16c2f79e905aa181e5e89
Kevin-BQ <kevinalexanderbricenoquezada@gmail.com>
2025-03-27 20:26:42 -0500

    feat(eureka,config,gateway): initial setup for microservice infrastructure

---
+++ msvc-gateway/src/test/java/kjo/care/msvc_gateway/MsvcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kjo.care.msvc_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
