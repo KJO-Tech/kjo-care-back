5f8f51304ff715ce9eaf77fabdb0d53d0f1c066a
fracuz1557 <jhonfrankx2811s@gmail.com>
2025-10-08 18:22:56 -0500

    feat(msvc-daily-activities): Add configuration for my microservice DTOs

---
+++ msvc-daily-activity/src/main/java/kjo/care/msvc_dailyActivity/DTOs/CategoryRequestDTO.java
@@ -0,0 +1,22 @@
+package kjo.care.msvc_dailyActivity.DTOs;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryRequestDTO {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres")
+    private String name;
+
+    @Size(max = 500, message = "La descripción no puede exceder 500 caracteres")
+    private String description;
+}
