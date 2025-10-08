36d71f6137b4c8ca4b797db380ea17090a27b7a8
fracuz1557 <jhonfrankx2811s@gmail.com>
2025-10-08 18:24:09 -0500

    feat(msvc-daily-activities): Add configuration for my microservice Mappers

---
+++ msvc-daily-activity/src/main/java/kjo/care/msvc_dailyActivity/Mappers/CategoryMapper.java
@@ -0,0 +1,49 @@
+package kjo.care.msvc_dailyActivity.Mappers;
+
+import kjo.care.msvc_dailyActivity.DTOs.CategoryRequestDTO;
+import kjo.care.msvc_dailyActivity.DTOs.CategoryResponseDTO;
+import kjo.care.msvc_dailyActivity.Entities.Category;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryMapper {
+
+    public Category toEntity(CategoryRequestDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        return Category.builder()
+                .name(dto.getName())
+                .description(dto.getDescription())
+                .build();
+    }
+
+    public CategoryResponseDTO toResponseDTO(Category entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        return CategoryResponseDTO.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .description(entity.getDescription())
+                .imageUrl(entity.getImageUrl())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .build();
+    }
+
+    public void updateEntityFromDTO(CategoryRequestDTO dto, Category entity) {
+        if (dto == null || entity == null) {
+            return;
+        }
+
+        if (dto.getName() != null) {
+            entity.setName(dto.getName());
+        }
+        if (dto.getDescription() != null) {
+            entity.setDescription(dto.getDescription());
+        }
+    }
+}
