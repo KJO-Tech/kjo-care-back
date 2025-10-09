5074b1eff5a5b27e2697b6ce40308d31751f9226
SebasVasRoj <the1shepard@gmail.com>
2025-10-08 21:37:09 -0500

    feat(msvc-emergency): health-center update findAll to support pagination and search

--- msvc-emergency/src/main/java/kjo/care/msvc_emergency/repositories/HealthRepository.java
+++ msvc-emergency/src/main/java/kjo/care/msvc_emergency/repositories/HealthRepository.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 import java.time.LocalDate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface HealthRepository extends JpaRepository<HealthCenter, UUID> {
     long countByStatus(StatusHealth status);
     int countByCreatedDateBetween(LocalDate startDate, LocalDate endDate);
+    Page<HealthCenter> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
